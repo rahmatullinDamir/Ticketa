e4ea08587a7dec36cd9ae0d928d5f3680d6e8f6b
Damir Rakhmatullin <145838629+rahmatullinDamir@users.noreply.github.com>
2026-03-15 22:32:43 +0300

    feat: add root services

---
+++ event-catalog-service/src/main/java/com/ticketa/eventcatalogservice/EventCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticketa.eventcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ notification-ticket-service/src/main/java/com/ticketa/notificationticketservice/NotificationTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticketa.notificationticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationTicketServiceApplication.class, args);
+	}
+
+}
