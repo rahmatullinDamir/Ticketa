1dd716af9e10008fc239d4a6d66a1b8214084556
Damir Rakhmatullin <145838629+rahmatullinDamir@users.noreply.github.com>
2026-04-06 01:56:59 +0300

    fix: edit security config and application.yml

--- auth-service/src/main/java/com/ticketa/authservice/security/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ticketa/authservice/security/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/fallback/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
